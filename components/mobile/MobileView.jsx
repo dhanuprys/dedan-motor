7f174423c71fc737a478ba9ec11de22092d34742
dhanuprys <dhanuprys@gmail.com>
2021-08-17 11:26:55 +0800

    Run logo testing

--- components/mobile/MobileView.jsx
+++ components/mobile/MobileView.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect } from 'react';
-import axiosFetcher from '../../utils/axios-fetcher';
+import Image from 'next/image';
+import styles from '../../styles/Home.module.css';
 
 export default function MobileView(props) {
-    
-
-    return "ME";
+    return (
+        <div className={styles.container}>
+            <Image src="/dedanmotor.svg" width="300" height="300" />
+        </div>
+    );
 }
