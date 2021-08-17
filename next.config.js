e39e9715423dfa0f310e92d04b7cf05b9a5e4412
dhanuprys <dhanuprys@gmail.com>
2021-08-17 11:29:26 +0800

    Install svg loader

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   reactStrictMode: true,
   useFileSystemPublicRoutes: true
 }
