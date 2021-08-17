2a505ab345ced0ee920e0f71a22c27b51c632e91
dhanuprys <dhanuprys@gmail.com>
2021-08-17 10:51:07 +0800

    Moving electron app to specified folder

--- electron/app.js
+++ electron/app.js
@@ -1,21 +1,6 @@
-const { env } = require('process');
-const { app, BrowserWindow } = require('electron');
+const { app } = require('electron');
 const nextServer = require('./next-server');
-
-function createWindow(port) {
-    const url = `https://localhost:${port}/`;
-    const isMobile = env.TEST_ENV === 'mobile';
-    const win = new BrowserWindow({
-        width: isMobile ? 414 : 800,
-        height: isMobile ? 736 : 600,
-        webPreferences: {
-            webSecurity: false,
-            allowRunningInsecureContent: true
-        }
-    });
-
-    win.loadURL(isMobile ? url + 'mobile' : url);
-}
+const { createWindow } = require('./window-manager');
 
 nextServer().then(connector => {
     console.log('Setup electron UI...');
---
+++ electron/window-manager.js
@@ -0,0 +1,17 @@
+const { env } = require('process');
+const { BrowserWindow } = require('electron');
+
+module.exports.createWindow = function createWindow(port) {
+    const url = `https://localhost:${port}/`;
+    const isMobile = env.TEST_ENV === 'mobile';
+    const win = new BrowserWindow({
+        width: isMobile ? 414 : 800,
+        height: isMobile ? 736 : 600,
+        webPreferences: {
+            webSecurity: false,
+            allowRunningInsecureContent: true
+        }
+    });
+
+    win.loadURL(isMobile ? url + 'mobile' : url);
+}
